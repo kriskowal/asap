cc90f852be8d29154ca96a2ed8ee7b27e6910a39
Kris Kowal <kris.kowal@cixar.com>
2014-05-26 08:17:46 -0700

    Follow-up: tune JSHint configuration, etc

--- test/browser-domain.js
+++ test/browser-domain.js
@@ -4,8 +4,8 @@ var EventEmitter = require("events").EventEmitter;
 var asap = require("../asap");
 
 // This is a very rudimentary domain shim meant only to work with asap and with
-// asap's tests. Unlike the real domain module, it requires manual teardown. It
-// uses `asap.setOnError` since `window.onerror` does not work very well.
+// asap's tests. Unlike the real domain module. Only one domain may be used at
+// a time. Since this is only used for testing, there is no cleanup.
 
 var active;
 
